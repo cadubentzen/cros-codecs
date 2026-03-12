aea17c88ea68ba5b320cbc80a1709ed6d80ce421
Carlos Bentzen <cadubentzen@igalia.com>
2026-03-12 21:44:48 +0100

    vaapi: fix wrong order of hrd parameters in h264 encoder

--- src/encoder/stateless/h264/vaapi.rs
+++ src/encoder/stateless/h264/vaapi.rs
@@ -556,7 +556,7 @@ where
             let hrd_buffer_fullness = hrd_buffer_size * 3 / 4;
 
             let hrd_param = BufferType::EncMiscParameter(libva::EncMiscParameter::HRD(
-                libva::EncMiscParameterHRD::new(hrd_buffer_size, hrd_buffer_fullness),
+                libva::EncMiscParameterHRD::new(hrd_buffer_fullness, hrd_buffer_size),
             ));
             picture.add_buffer(self.context().create_buffer(hrd_param)?);
         }
